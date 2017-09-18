69599dafda82c80928d76b3e18246691072072ff
wanggene <onedna@gmail.com>
2017-09-18 01:30:41 -0400

    update folder address

--- global.R
+++ global.R
@@ -1,6 +1,6 @@
 #global.R
 library(data.table)
-setwd("~/GitHub/citybike")
+setwd("~/GitHub/shiny_citybike")
 citibike = fread(file = "../Data/citibike_all.csv")
 file_all = citibike %>% 
     filter(Gender %in% c(1, 2)) %>%
